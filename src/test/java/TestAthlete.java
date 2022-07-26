c37dd2c65e1eda37b5f3d84fb31c985a9a464384
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2022-07-26 14:06:51 +0300

    Update TestAthlete.java

--- src/test/java/TestAthlete.java
+++ src/test/java/TestAthlete.java
@@ -13,27 +13,27 @@ public class TestGrade {
   {
     Athlete a1 = new Athlete("Linoy Ashram", 4);
     assertEquals(a1.getName(), "Linoy Ashram");
-    Grade g1 = new Grade("Clubs", 24.1);
-    Grade g2 = new Grade("Ribbon", 25.1);
+    Grade g1 = new Grade("Clubs", 24);
+    Grade g2 = new Grade("Ribbon", 25);
     boolean b = a1.addGrade(g1);
     assertEquals(b, true);
     a1.addGrade(g2);
     double d = a1.average();
-    assertEquals(d, 24.6);
+    assertEquals(d, 24.5);
     b = a1.allAbove(20);
     assertEquals(b, true);
 
     Athlete a2 = new Athlete(a1);
     a2.setName("Dina averina");
     a2.delete("Ribbon");
-    g1 = new Grade("Hoop", 23.8);
+    g1 = new Grade("Hoop", 23);
     a2.addGrade(g1);
     b = a1.allAbove(24);
     assertEquals(b, false);
     
     assertEquals(a1.surpasses(a2), false);    
     a2.delete("Clubs");
-    a2.addGrade(new Grade("Clubs", 23.1));
+    a2.addGrade(new Grade("Clubs", 23));
     assertEquals(a1.surpasses(a2), true);    
    }
 }
